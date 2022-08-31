8a42722384d175ada0020e142be2bb4bab12f9da
a.kazemi <a.kazemi@dataak.com>
2022-08-31 21:56:27 +0430

    updated services

--- main.go
+++ main.go
@@ -11,8 +11,13 @@ type MyChartProps struct {
 	cdk8s.ChartProps
 }
 
+type NginxChartProps struct {
+	cdk8s.ChartProps
+}
+
 var sharedPVC cdk8splus22.PersistentVolumeClaim
 
+// MySQL Chart
 func NewMySQLChart(scope constructs.Construct, id string, props *MyChartProps) cdk8s.Chart {
 	var cprops cdk8s.ChartProps
 	if props != nil {
@@ -31,15 +36,17 @@ func NewMySQLChart(scope constructs.Construct, id string, props *MyChartProps) c
 	secretKey := "VerySecureSecretKey123"
 	mysqlSecret.AddStringData(jsii.String(secretKey), jsii.String(password))
 
+	deploymentName := "nextcloud"
+
 	// MySQL Deployment
-	mysqldep := cdk8splus22.NewDeployment(chart, jsii.String("mysql-deployment-cdk8splus"), &cdk8splus22.DeploymentProps{})
+	mysqldep := cdk8splus22.NewDeployment(chart, jsii.String("mysql-deployment"), &cdk8splus22.DeploymentProps{Metadata: &cdk8s.ApiObjectMetadata{Labels: &map[string]*string{"app": &deploymentName}}})
 
 	containerImage := "mysql"
 
 	mysqlContainer := mysqldep.AddContainer(&cdk8splus22.ContainerProps{
 		Name:  jsii.String("mysql-container"),
 		Image: jsii.String(containerImage),
-		Port:  jsii.Number(3306),
+		// Port:  jsii.Number(3306),
 	})
 
 	// Using Secret in Env
@@ -54,22 +61,26 @@ func NewMySQLChart(scope constructs.Construct, id string, props *MyChartProps) c
 	mysqlVolume := cdk8splus22.Volume_FromPersistentVolumeClaim(chart, jsii.String("mysql-vol-pvc"), sharedPVC, &cdk8splus22.PersistentVolumeClaimVolumeOptions{Name: jsii.String(mysqlVolumeName)})
 
 	mysqlVolumeMountPath := "/var/lib/mysql"
-	mysqlContainer.Mount(jsii.String(mysqlVolumeMountPath), mysqlVolume, &cdk8splus22.MountOptions{})
+	mysqlVolumeMountSubPath := "server-data"
+
+	mysqlContainer.Mount(jsii.String(mysqlVolumeMountPath), mysqlVolume, &cdk8splus22.MountOptions{SubPath: &mysqlVolumeMountSubPath})
 
 	// Creating Service for MySQL Deployment
 	mysqlServiceName := "mysql-service"
-	clusterIPNone := "None"
+	// clusterIPNone := "None"
+	appName := "nextcloud"
 
 	cdk8splus22.NewService(chart, jsii.String("mysql-service"), &cdk8splus22.ServiceProps{
-		Metadata:  &cdk8s.ApiObjectMetadata{Name: jsii.String(mysqlServiceName)},
-		Selector:  mysqldep,
-		ClusterIP: jsii.String(clusterIPNone),
-		Ports:     &[]*cdk8splus22.ServicePort{{Port: jsii.Number(3306)}},
+		Metadata: &cdk8s.ApiObjectMetadata{Name: jsii.String(mysqlServiceName), Labels: &map[string]*string{"app": &appName}},
+		Selector: mysqldep,
+		// ClusterIP: jsii.String(clusterIPNone),
+		Ports: &[]*cdk8splus22.ServicePort{{Port: jsii.Number(3306)}},
 	})
 
 	return chart
 }
 
+// Nextcloud Chart
 func NewNextCloudChart(scope constructs.Construct, id string, props *MyChartProps) cdk8s.Chart {
 	var cprops cdk8s.ChartProps
 	if props != nil {
@@ -77,15 +88,16 @@ func NewNextCloudChart(scope constructs.Construct, id string, props *MyChartProp
 	}
 	chart := cdk8s.NewChart(scope, jsii.String(id), &cprops)
 
+	deploymentName := "nextcloud"
 	// NextCloud Deployment
-	nextclouddep := cdk8splus22.NewDeployment(chart, jsii.String("nextcloud-deployment-cdk8splus"), &cdk8splus22.DeploymentProps{})
+	nextclouddep := cdk8splus22.NewDeployment(chart, jsii.String("nextcloud-deployment"), &cdk8splus22.DeploymentProps{Metadata: &cdk8s.ApiObjectMetadata{Labels: &map[string]*string{"app": &deploymentName}}})
 
 	containerImage := "nextcloud:16-apache"
 
 	nextcloudContainer := nextclouddep.AddContainer(&cdk8splus22.ContainerProps{
 		Name:  jsii.String("nextcloud-container"),
 		Image: jsii.String(containerImage),
-		Port:  jsii.Number(80),
+		// Port:  jsii.Number(80),
 	})
 
 	// Using Shared PVC
@@ -93,21 +105,25 @@ func NewNextCloudChart(scope constructs.Construct, id string, props *MyChartProp
 	nextcloudVolume := cdk8splus22.Volume_FromPersistentVolumeClaim(chart, jsii.String("nextcloud-vol-pvc"), sharedPVC, &cdk8splus22.PersistentVolumeClaimVolumeOptions{Name: jsii.String(nextcloudVolumeName)})
 
 	nextcloudVolumeMountPath := "/var/www/html"
-	nextcloudContainer.Mount(jsii.String(nextcloudVolumeMountPath), nextcloudVolume, &cdk8splus22.MountOptions{})
+	nextcloudVolumeMountSubPath := "server-data"
+
+	nextcloudContainer.Mount(jsii.String(nextcloudVolumeMountPath), nextcloudVolume, &cdk8splus22.MountOptions{SubPath: &nextcloudVolumeMountSubPath})
 
 	nextcloudServiceName := "nextcloud-service"
-	clusterIPNone := "None"
+	// clusterIPNone := "None"
+	appName := "nextcloud"
 
 	cdk8splus22.NewService(chart, jsii.String("nextcloud-service"), &cdk8splus22.ServiceProps{
-		Metadata:  &cdk8s.ApiObjectMetadata{Name: jsii.String(nextcloudServiceName)},
-		Selector:  nextclouddep,
-		ClusterIP: jsii.String(clusterIPNone),
-		Ports:     &[]*cdk8splus22.ServicePort{{Port: jsii.Number(3306)}},
+		Metadata: &cdk8s.ApiObjectMetadata{Name: jsii.String(nextcloudServiceName), Labels: &map[string]*string{"app": &appName}},
+		Selector: nextclouddep,
+		// ClusterIP: jsii.String(clusterIPNone),
+		Ports: &[]*cdk8splus22.ServicePort{{Port: jsii.Number(80)}},
 	})
 
 	return chart
 }
 
+// PVC Chart
 func NewPVCChart(scope constructs.Construct, id string, props *MyChartProps) cdk8s.Chart {
 	var cprops cdk8s.ChartProps
 	if props != nil {
@@ -115,25 +131,71 @@ func NewPVCChart(scope constructs.Construct, id string, props *MyChartProps) cdk
 	}
 	chart := cdk8s.NewChart(scope, jsii.String(id), &cprops)
 
+	pvcName := "shared-pvc"
+	appName := "nextcloud"
 	storageClassType := "rawfile-btrfs"
 
 	sharedPVC = cdk8splus22.NewPersistentVolumeClaim(chart, jsii.String("shared-pvc"), &cdk8splus22.PersistentVolumeClaimProps{
+		Metadata:         &cdk8s.ApiObjectMetadata{Name: jsii.String(pvcName), Labels: &map[string]*string{"app": &appName}},
 		AccessModes:      &[]cdk8splus22.PersistentVolumeAccessMode{cdk8splus22.PersistentVolumeAccessMode_READ_WRITE_ONCE},
-		Storage:          cdk8s.Size_Mebibytes(jsii.Number(512)),
+		Storage:          cdk8s.Size_Gibibytes(jsii.Number(1)),
 		StorageClassName: &storageClassType,
+		VolumeMode:       cdk8splus22.PersistentVolumeMode_FILE_SYSTEM,
 	})
 
+	return chart
+}
+
+// Nginx Chart
+func NewNginxChart(scope constructs.Construct, id string, props *NginxChartProps) cdk8s.Chart {
+	var cprops cdk8s.ChartProps
+	if props != nil {
+		cprops = props.ChartProps
+	}
+	chart := cdk8s.NewChart(scope, jsii.String(id), &cprops)
+
+	nginxdep := cdk8splus22.NewDeployment(chart, jsii.String("deployment"), &cdk8splus22.DeploymentProps{Metadata: &cdk8s.ApiObjectMetadata{Name: jsii.String("nginx-deployment")}})
+
+	nginxdep.AddContainer(&cdk8splus22.ContainerProps{
+		Name:  jsii.String("nginx-container"),
+		Image: jsii.String("nginx"),
+		// Port:  jsii.Number(80),
+	})
+
+	nginxdep.ExposeViaService(&cdk8splus22.DeploymentExposeViaServiceOptions{
+		Name:        jsii.String("nginx-container-service"),
+		ServiceType: cdk8splus22.ServiceType_LOAD_BALANCER,
+		Ports:       &[]*cdk8splus22.ServicePort{{Port: jsii.Number(9090), TargetPort: jsii.Number(80)}}})
+
+	return chart
+}
+
+// Ingress Chart
+func NewIngressChart(scope constructs.Construct, id string, props *MyChartProps) cdk8s.Chart {
+	var cprops cdk8s.ChartProps
+	if props != nil {
+		cprops = props.ChartProps
+	}
+	chart := cdk8s.NewChart(scope, jsii.String(id), &cprops)
+
+	ingressName := "cluster-ingress"
+	host := "nextcloud.site.test"
+	// NextCloud Deployment
+	nginxingress := cdk8splus22.NewIngress(chart, jsii.String("nextcloud-deployment"), &cdk8splus22.IngressProps{Metadata: &cdk8s.ApiObjectMetadata{Labels: &map[string]*string{"app": &ingressName}}})
+	nginxingress.AddRules(&cdk8splus22.IngressRule{Host: &host})
+	nginxingress.AddDefaultBackend(cdk8splus22.IngressBackend_FromService)
 	return chart
 }
 
 func main() {
 	app := cdk8s.NewApp(nil)
 
+	ingressChart := NewIngressChart(app, "ingress", nil)
 	sharedPVCChart := NewPVCChart(app, "pvc", nil)
 	mysqlChart := NewMySQLChart(app, "mysql", nil)
-	mysqlChart.AddDependency(sharedPVCChart)
 	nextcloudChart := NewNextCloudChart(app, "nextcloud", nil)
-	nextcloudChart.AddDependency(sharedPVCChart, mysqlChart)
-
+	nginxChart := NewNginxChart(app, "nginx", nil)
+	mysqlChart.AddDependency(sharedPVCChart, nginxChart, ingressChart)
+	nextcloudChart.AddDependency(sharedPVCChart, mysqlChart, nginxChart, ingressChart)
 	app.Synth()
 }
